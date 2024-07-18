78fe7c1483c45501ba13a9c1101d9a09b3e6ec26
Hitanshu <galahitanshu@gmail.com>
2024-07-19 01:11:11 +0530

    ccomit 3

--- backend/config.js
+++ backend/config.js
@@ -4,5 +4,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const PORT = process.env.PORT || 5555;
-export const mongodburl = process.env.mongodburl;
+export const mongodburl = 'mongodb+srv://root:root@katha-kritique.mdjws7b.mongodb.net/test?retryWrites=true&w=majority';
 export const googleAPIKey = process.env.googleAPIKey;
