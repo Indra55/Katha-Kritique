96a7a6f2b8d06ea506b735091e01bd665470cabf
Hitanshu Gala <121685112+Indra55@users.noreply.github.com>
2025-01-02 23:21:11 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -4,5 +4,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const PORT = process.env.PORT || 5555;
-export const mongodburl = 'mongodb+srv://root:root@katha-kritique.mdjws7b.mongodb.net/test?retryWrites=true&w=majority';
-export const googleAPIKey = process.env.googleAPIKey;
+export const mongodburl = process.env.MONGODBURL;
+export const googleAPIKey = process.env.GOOGLEAPIKEY;
+
